ba787322fed74633fe30401bbe6d9b6de8437d95
Alan Joyce <a.joyce@kyrio.com>
2019-09-21 18:33:47 -0400

    Updated test fixture to clean up the queue. When testing a fifo queue.

--- test/Queues/MessageQueueFixture.cs
+++ test/Queues/MessageQueueFixture.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Xunit;
 using PipServices3.Messaging.Queues;
+using PipServices3.Commons.Data;
 
 namespace PipServices3.Aws.Queues
 {
@@ -18,7 +19,7 @@ namespace PipServices3.Aws.Queues
 
         public async Task TestSendReceiveMessageAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
             await _queue.SendAsync(null, envelope1);
 
             var count = _queue.MessageCount;
@@ -29,11 +30,14 @@ namespace PipServices3.Aws.Queues
             Assert.Equal(envelope1.MessageType, envelope2.MessageType);
             Assert.Equal(envelope1.Message, envelope2.Message);
             Assert.Equal(envelope1.CorrelationId, envelope2.CorrelationId);
+
+            if (_isFifo)
+                await _queue.CompleteAsync(envelope2);
         }
 
         public async Task TestMoveToDeadMessageAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
             await _queue.SendAsync(null, envelope1);
 
             var envelope2 = await _queue.ReceiveAsync(null, 10000);
@@ -47,9 +51,10 @@ namespace PipServices3.Aws.Queues
 
         public async Task TestReceiveSendMessageAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
 
-            ThreadPool.QueueUserWorkItem(async delegate {
+            ThreadPool.QueueUserWorkItem(async delegate
+            {
                 Thread.Sleep(500);
                 await _queue.SendAsync(null, envelope1);
             });
@@ -59,11 +64,14 @@ namespace PipServices3.Aws.Queues
             Assert.Equal(envelope1.MessageType, envelope2.MessageType);
             Assert.Equal(envelope1.Message, envelope2.Message);
             Assert.Equal(envelope1.CorrelationId, envelope2.CorrelationId);
+
+            if (_isFifo)
+                await _queue.CompleteAsync(envelope2);
         }
 
         public async Task TestReceiveAndCompleteMessageAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
             await _queue.SendAsync(null, envelope1);
             var envelope2 = await _queue.ReceiveAsync(null, 10000);
             Assert.NotNull(envelope2);
@@ -78,7 +86,7 @@ namespace PipServices3.Aws.Queues
 
         public async Task TestReceiveAndAbandonMessageAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
             await _queue.SendAsync(null, envelope1);
             var envelope2 = await _queue.ReceiveAsync(null, 10000);
             Assert.NotNull(envelope2);
@@ -92,18 +100,28 @@ namespace PipServices3.Aws.Queues
             Assert.Equal(envelope1.MessageType, envelope2.MessageType);
             Assert.Equal(envelope1.Message, envelope2.Message);
             Assert.Equal(envelope1.CorrelationId, envelope2.CorrelationId);
+
+            if (_isFifo)
+                await _queue.CompleteAsync(envelope2);
         }
 
         public async Task TestSendPeekMessageAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
             await _queue.SendAsync(null, envelope1);
             await Task.Delay(500);
+
             var envelope2 = await _queue.PeekAsync(null);
             Assert.NotNull(envelope2);
             Assert.Equal(envelope1.MessageType, envelope2.MessageType);
             Assert.Equal(envelope1.Message, envelope2.Message);
             Assert.Equal(envelope1.CorrelationId, envelope2.CorrelationId);
+
+            if (_isFifo)
+            {
+                envelope2 = await _queue.ReceiveAsync(null, 10000);
+                await _queue.CompleteAsync(envelope2);
+            }
         }
 
         public async Task TestPeekNoMessageAsync()
@@ -114,20 +132,28 @@ namespace PipServices3.Aws.Queues
 
         public async Task TestMessageCountAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
             await _queue.SendAsync(null, envelope1);
             await Task.Delay(500);
+
             var count = _queue.MessageCount;
             Assert.NotNull(count);
             Assert.True(count.Value >= 1);
+
+            if (_isFifo)
+            {
+                var envelope2 = await _queue.ReceiveAsync(null, 10000);
+                await _queue.CompleteAsync(envelope2);
+            }
         }
 
         public async Task TestOnMessageAsync()
         {
-            var envelope1 = new MessageEnvelope("123", "Test", "Test message");
+            var envelope1 = new MessageEnvelope(IdGenerator.NextLong(), "Test", "Test message");
             MessageEnvelope envelope2 = null;
 
-            _queue.BeginListen(null, async (envelope, queue) => {
+            _queue.BeginListen(null, async (envelope, queue) =>
+            {
                 envelope2 = envelope;
                 await Task.Delay(0);
             });
@@ -140,8 +166,10 @@ namespace PipServices3.Aws.Queues
             Assert.Equal(envelope1.Message, envelope2.Message);
             Assert.Equal(envelope1.CorrelationId, envelope2.CorrelationId);
 
+            if (_isFifo)
+                await _queue.CompleteAsync(envelope2);
+
             await _queue.CloseAsync(null);
         }
-
     }
 }
