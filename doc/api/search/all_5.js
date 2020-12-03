19056a83360f18bf1b71c2136ca6bf39fd167ffb
banalna <judas.priest999@gmail.com>
2020-12-03 15:28:26 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['isopen',['IsOpen',['../class_pip_services_1_1_aws_1_1_count_1_1_cloud_watch_counters.html#aa17d46373e86660c1fb2a3041c0a3066',1,'PipServices3.Aws.Count.CloudWatchCounters.IsOpen()'],['../class_pip_services_1_1_aws_1_1_log_1_1_cloud_watch_logger.html#a698c1c33ef409b21faf39f00fc601dff',1,'PipServices3.Aws.Log.CloudWatchLogger.IsOpen()']]]
-];
